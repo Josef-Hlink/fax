2b40e480a01a27dcd396e9dbc96e13e2250dcd04
Josef Hamelink <91252200+Josef-Hlink@users.noreply.github.com>
2025-05-16 14:17:52 +0200

    Improve readability of example

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python3
-# uv run main.py -e ~/Vault/fastslp.app -i ~/Vault/melee.iso -r ~/Desktop/
+#!/usr/bin/env python3
+# minimal standalone example of how to use libmelee with headless slippi
+# uv run example.py -e ~/Vault/fastslp.app -i ~/Vault/melee.iso -r ~/Desktop/
 
 import argparse
 import signal
@@ -10,14 +11,14 @@ import melee
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--exe", "-e", type=str)
-    parser.add_argument("--iso", "-i", type=str)
-    parser.add_argument("--rep", "-r", type=str)
+    parser.add_argument('--exe', '-e', type=str, help='slippi executable (.app)')
+    parser.add_argument('--iso', '-i', type=str, help='melee iso')
+    parser.add_argument('--rep', '-r', type=str, help='where to save replays')
     args = parser.parse_args()
 
     console = melee.Console(
         path=args.exe,
-        gfx_backend="Null",
+        gfx_backend='Null',
         disable_audio=True,
         use_exi_inputs=True,
         enable_ffw=True,
@@ -38,34 +39,32 @@ def main():
     console.run(iso_path=args.iso)
     if not console.connect():
         sys.exit(1)
-
     if not contr1.connect():
         sys.exit(1)
     if not contr2.connect():
         sys.exit(1)
 
-    # framedata = melee.framedata.FrameData()
     menu_helper = melee.MenuHelper()
     fox, yoshis = melee.Character.FOX, melee.Stage.YOSHIS_STORY
     last_frame = -999
     while True:
-        gs = console.step()
+        gs = console.step()  # gs = game state
         if gs is None:
             continue
         if gs.menu_state in [melee.Menu.IN_GAME, melee.Menu.SUDDEN_DEATH]:
             if gs.frame < last_frame:
-                print("game over, exiting loop")
+                print('game over, exiting loop')
                 console.stop()
                 sys.exit(0)
             last_frame = gs.frame
-            print(f"\r{gs.frame}", end="")
+            print(f'\r{gs.frame}', end='')
             # CONTROLLER MANIPULATION GOES HERE
-            contr1.tilt_analog(melee.enums.Button.BUTTON_MAIN, 1, 0.5)
-            contr2.tilt_analog(melee.enums.Button.BUTTON_MAIN, 0, 0.5)
+            contr1.tilt_analog(melee.enums.Button.BUTTON_MAIN, 1, 0.5)  # walk right
+            contr2.tilt_analog(melee.enums.Button.BUTTON_MAIN, 0, 0.5)  # walk left
         else:  # we're in menus
-            menu_helper.menu_helper_simple(gs, contr1, fox, yoshis, "", 0, 1, True)
-            menu_helper.menu_helper_simple(gs, contr2, fox, yoshis, "", 0, 2, False)
+            menu_helper.menu_helper_simple(gs, contr1, fox, yoshis, '', 0, 1, True)
+            menu_helper.menu_helper_simple(gs, contr2, fox, yoshis, '', 0, 2, False)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
