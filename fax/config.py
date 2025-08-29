8034125206e8bef4be6ae685c05d7e1df2d256ff
Josef Hamelink <91252200+Josef-Hlink@users.noreply.github.com>
2025-08-30 00:30:12 +0200

    create barebones config argparser
    simple test included in __name__ block

---
+++ fax/config.py
@@ -0,0 +1,91 @@
+from typing import Type, Any, Tuple, Dict
+from argparse import ArgumentParser, Namespace
+
+import attr
+
+
+@attr.s(auto_attribs=True, frozen=True)
+class BaseConfig:
+    seed: int = 42
+    n_gpus: int = 1
+    debug: bool = False
+
+
+@attr.s(auto_attribs=True, frozen=True)
+class DataConfig:
+    """Separated for easy passing around."""
+
+    dir: str = ''
+    seq_len: int = 2**8
+    max_samples: int = -1  # -1 means no limit
+
+
+@attr.s(auto_attribs=True, frozen=True)
+class TrainConfig:
+    """Inherits from BaseConfig."""
+
+    data: DataConfig = DataConfig()
+    batch_size: int = 64
+    lr: float = 3e-4
+    n_samples: int = 2**10
+    n_eval_samples: int = 2**8
+    n_epochs: int = 4
+    beta1: float = 0.9
+    beta2: float = 0.999
+    wd: float = 1e-2  # weight decay
+    grad_clip_norm: float = 1.0
+
+    @property
+    def betas(self) -> Tuple[float, float]:
+        return (self.beta1, self.beta2)
+
+
+def create_parser(cls: Type[Any], parser: ArgumentParser, prefix: str = '') -> ArgumentParser:
+    for field in attr.fields(cls):
+        arg_name = f'--{prefix}{field.name.replace("_", "-")}'
+        if attr.has(field.type):
+            # nested dataclass -> recurse
+            parser = create_parser(field.type, parser, prefix=f'{field.name}.')
+        else:
+            if field.type == bool:
+                parser.add_argument(
+                    arg_name, action='store_true', help=field.metadata.get('help', '')
+                )
+            else:
+                parser.add_argument(
+                    arg_name,
+                    type=field.type,
+                    default=field.default if field.default is not attr.NOTHING else None,
+                    help=field.metadata.get('help', ''),
+                    required=field.default is attr.NOTHING,
+                )
+
+    return parser
+
+
+def parse_args(cls: Type[Any], args: Namespace, prefix: str = '') -> Any:
+    kwargs: Dict[str, Any] = {}
+    for field in attr.fields(cls):
+        arg_name = f'{prefix}{field.name.replace("_", "-")}'
+        if attr.has(field.type):
+            # nested dataclass -> recurse
+            value = parse_args(field.type, args, prefix=f'{field.name}.')
+        else:
+            value = getattr(args, arg_name.replace('-', '_'), field.default)
+            if value is None and field.default is attr.NOTHING:
+                raise ValueError(f'Missing required argument: {arg_name}')
+        kwargs[field.name] = value
+    return cls(**kwargs)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser = create_parser(BaseConfig, parser)
+    parser = create_parser(TrainConfig, parser, prefix='train.')
+    args = parser.parse_args()
+
+    base_config = parse_args(BaseConfig, args)
+    train_config = parse_args(TrainConfig, args, prefix='train.')
+
+    print(base_config)
+    print(train_config)
