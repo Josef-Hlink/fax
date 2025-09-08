0fe5aad0d81c475c3a759bc5e10aac466ff5c19f
Josef Hamelink <91252200+Josef-Hlink@users.noreply.github.com>
2025-09-08 13:43:42 +0200

    done: slp to mds pipeline functional
    only some perimeter stuff left to do here and then we can start training

--- fax/slp_to_mds.py
+++ fax/slp_to_mds.py
@@ -5,24 +5,37 @@
 This script converts a directory of .slp files into an MDS dataset.
 """
 
+import hashlib
+import random
+import struct
+import multiprocessing as mp
 from itertools import islice
+from collections import defaultdict
+from typing import Any, Dict, Optional
 from typing import List
 from pathlib import Path
 
+import melee
+import numpy as np
 from loguru import logger
 from tqdm import tqdm
+from streaming import MDSWriter
 
 from fax.utils import debug_enabled
 from fax.database import DataBase
 from fax.slp_reader import parse_replay
+from fax.constants import NP_MASK_VALUE
+from fax.schema import MDS_DTYPE_STR_BY_COLUMN, NP_TYPE_BY_COLUMN
+from fax.gamestate_utils import FrameData, extract_and_append_gamestate_inplace
 
 
-def main(slp_dir: Path, db_path: Path, n: int = -1) -> None:
-    # db = setup_database(db_path)
-    # parse_and_store(slp_dir, db, n)
-    db = DataBase(db_path)
-    # remove_faulty_replays(slp_dir, db.get_faulty_replays())
+def main(slp_dir: Path, db_path: Path, mds_dir: Path, n: int, w: int) -> None:
+    db = setup_database(db_path)
+    parse_and_store(slp_dir, db, n)
+    remove_faulty_replays(slp_dir, db.get_faulty_replays())
     fox, nonfox = split_fox_nonfox_files(slp_dir, db)
+    process_replays([slp_dir / f for f in fox], mds_dir / 'fox', n_workers=w)
+    process_replays([slp_dir / f for f in nonfox], mds_dir / 'nonfox', n_workers=w)
     return
 
 
@@ -111,6 +124,109 @@ def split_fox_nonfox_files(slp_dir: Path, db: DataBase) -> tuple[set[str], set[s
     return valid_fox_files, valid_nonfox_files
 
 
+def hash_to_int32(data: str) -> int:
+    hash_bytes = hashlib.md5(data.encode()).digest()  # Get 16-byte hash
+    int32_value = struct.unpack('i', hash_bytes[:4])[0]  # Convert first 4 bytes to int32
+    return int32_value
+
+
+def split_train_val(input_paths: List[Path], split: float = 0.95) -> Dict[str, List[Path]]:
+    """Split input paths into train and validation sets."""
+    split_idx = int(len(input_paths) * split)
+    return {'train': input_paths[:split_idx], 'val': input_paths[split_idx:]}
+
+
+def process_replay(replay_path: Path) -> Optional[Dict[str, Any]]:
+    """Process a single replay file and extract frame data.
+    Args:
+        replay_path: Path to the .slp replay file.
+    Returns:
+        A dictionary containing frame data arrays, or None if processing failed.
+
+    Largely adapted from https://github.com/ericyuegu/hal
+    """
+    frame_data: FrameData = defaultdict(list)
+    try:
+        console = melee.Console(path=str(replay_path), is_dolphin=False, allow_old_version=True)
+        console.connect()
+    except Exception as e:
+        logger.debug(f'Error connecting to console for {replay_path}: {e}')
+        return None
+
+    replay_uuid = hash_to_int32(str(replay_path))
+
+    try:
+        # Double step on first frame to match next controller state to current gamestate
+        curr_gamestate = console.step()
+        while curr_gamestate is not None:
+            next_gamestate = console.step()
+            frame_data = extract_and_append_gamestate_inplace(
+                frame_data_by_field=frame_data,
+                curr_gamestate=curr_gamestate,
+                next_gamestate=next_gamestate,
+                replay_uuid=replay_uuid,
+            )
+            curr_gamestate = next_gamestate
+            if curr_gamestate is None:
+                break
+    except AssertionError as e:
+        logger.trace(f'Skipping replay {replay_path}: {e}')
+        return None
+    except Exception as e:
+        logger.debug(f'Error processing replay {replay_path}: {e}')
+        return None
+    finally:
+        console.stop()
+
+    sample = {}
+    for key, dtype in NP_TYPE_BY_COLUMN.items():
+        if key in frame_data:
+            array = [x if x is not None else NP_MASK_VALUE for x in frame_data[key]]
+            sample[key] = np.array(array, dtype=dtype)
+
+    sample['replay_uuid'] = np.array([replay_uuid] * len(frame_data['frame']), dtype=np.int32)
+    return sample
+
+
+def process_replays(replay_paths: list[Path], mds_dir: Path, n_workers: int) -> None:
+    """Process a list of replay files into an MDS dataset.
+    Args:
+        replay_paths: List of paths to .slp replay files.
+        mds_dir: Directory where the MDS dataset will be created.
+        n_workers: Number of MP worker processes to use for processing replays.
+    """
+
+    logger.info(f'Processing {len(replay_paths)} replays into MDS dataset at {mds_dir}...')
+    random.shuffle(replay_paths)
+
+    splits = split_train_val(input_paths=replay_paths, split=0.95)
+    for split, split_replay_paths in splits.items():
+        split_output_dir = mds_dir / split
+        split_output_dir.mkdir(parents=True, exist_ok=True)
+
+        if (n_replays := len(split_replay_paths)) == 0:
+            logger.info(f'No replays found for {split} split')
+            continue
+
+        logger.info(f'Writing {n_replays} replays to {split_output_dir}')
+        actual = 0
+        with MDSWriter(
+            out=str(split_output_dir),
+            columns=MDS_DTYPE_STR_BY_COLUMN,
+            compression='zstd',
+            size_limit=1 << 31,  # Write 2GB shards, data is repetitive so compression is 10-20x
+            exist_ok=True,
+        ) as out:
+            with mp.Pool(n_workers) as pool:
+                samples = pool.imap_unordered(process_replay, split_replay_paths)
+                for sample in tqdm(samples, total=n_replays, desc=f'Processing {split} split'):
+                    if sample is not None:
+                        out.write(sample)
+                        actual += 1
+        logger.info(f'Wrote {actual} replays ({actual / n_replays:.2%}) to {split_output_dir}')
+    return
+
+
 if __name__ == '__main__':
     import sys
     from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
@@ -126,17 +242,37 @@ if __name__ == '__main__':
         help='Directory containing .slp files to index.',
     )
     parser.add_argument(
+        '--db_path',
+        type=Path,
+        default=PROJECT_ROOT / 'db.sqlite',
+        help='Path where the SQLite database will be created. '
+        + 'Defaults to <project_root>/db.sqlite.',
+    )
+    parser.add_argument(
+        '--mds_dir',
+        type=Path,
+        default=PROJECT_ROOT / 'data' / 'mds',
+        help='Directory where the MDS dataset will be created. '
+        + 'Defaults to <project_root>/data/mds.',
+    )
+    parser.add_argument(
         '-n',
         type=int,
         default=-1,
         help='Number of files to process. Default is -1 (process all files).',
     )
     parser.add_argument(
-        '--db_path',
-        type=Path,
-        default=PROJECT_ROOT / 'db.sqlite',
-        help='Path where the SQLite database will be created. '
-        + 'Defaults to <project_root>/db.sqlite.',
+        '-w',
+        type=int,
+        default=max(1, mp.cpu_count() - 1),
+        help='Number of MP workers processes to use for processing replays. '
+        + 'Defaults to number of CPU cores minus one.',
+    )
+    parser.add_argument(
+        '--seed',
+        type=int,
+        default=42,
+        help='Random seed for shuffling replays before processing.',
     )
     parser.add_argument('-D', '--debug', action='store_true', help='Enable debug mode.')
     args = parser.parse_args()
@@ -145,14 +281,23 @@ if __name__ == '__main__':
     slp_dir = args.slp_dir.expanduser().resolve()
     assert slp_dir.is_dir(), f'{slp_dir} is not a directory'
     assert any(slp_dir.rglob('*.slp')), f'No .slp files found in {slp_dir}'
+
     db_path = args.db_path.expanduser().resolve()
-    # if db_path.exists():
-    #     logger.error(f'{db_path} already exists; please delete it first')
-    #     sys.exit(1)
+    if db_path.exists():
+        logger.error(f'{db_path} already exists; please delete it first')
+        sys.exit(1)
+
+    mds_dir = args.mds_dir.expanduser().resolve()
+    if mds_dir.exists() and any(mds_dir.iterdir()):
+        logger.error(f'{mds_dir} already exists and has contents; please delete it first')
+        sys.exit(1)
+    mds_dir.mkdir(parents=True, exist_ok=True)
+
+    random.seed(args.seed)
 
     # set up logging
     logger.remove()
     logger.add(sys.stderr, level='DEBUG' if args.debug else 'INFO')
     logger.debug('Debug mode enabled')
 
-    main(slp_dir, db_path, args.n)
+    main(slp_dir, db_path, mds_dir, args.n, args.w)
